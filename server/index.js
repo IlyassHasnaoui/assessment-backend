73e974dfeb87f54ca95a331457f8005f918c7c3c
“Ilyass <ilyass.has@live.fr>
2023-10-19 18:56:45 -0400

    new update

--- server/index.js
+++ server/index.js
@@ -7,20 +7,12 @@ app.use(cors());
 
 app.use(express.json());
 
-const { getCompliment } = require('./controller')
+const { getCompliment, getFortune, addGoal, getNumber, getQuoteOfTheDay} = require('./controller')
 
-app.get("/api/compliment", getCompliment);
-
-app.listen(4000, () => console.log("Server running on 4000"));
-
-const { getFortune } = require('./controller')
-
+app.get("/api/get", getCompliment);
 app.get("/api/fortune", getFortune);
-
-app.listen(4000, () => console.log("Server running on 4000"));
-
-const { getNumber } = require('./Number')
-
 app.get("/api/number", getNumber);
+app.put("/api/addGoal", addGoal);
+app.post("/api/post", getQuoteOfTheDay);
 
 app.listen(4000, () => console.log("Server running on 4000"));
